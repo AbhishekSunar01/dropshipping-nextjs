f74c7c5c020ba14f30fa4d1eb05b1fa67f9672af
Abhishek Sunar <a8hishek01@gmail.com>
2023-12-03 12:47:59 +0545

    first commit

--- app/pages/orders/page.jsx
+++ app/pages/orders/page.jsx
@@ -1,5 +1,149 @@
+import SearchBar from "@/app/components/SearchBar";
+
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div>
+      <SearchBar />
+
+      <table className="w-full mt-10">
+        <thead>
+          <tr className="text-sm">
+            <th className="p-2">Product</th>
+            <th className="p-2">Date</th>
+            <th className="p-2">Item ID</th>
+            <th className="p-2">Supplier</th>
+            <th className="p-2">QTY</th>
+            <th className="p-2">Customer</th>
+            <th className="p-2">Address</th>
+            <th className="p-2">Price</th>
+            <th className="p-2">Status</th>
+            <th className="p-2">More</th>
+          </tr>
+        </thead>
+        <tbody className="text-sm text-center text-secondary border-t-[1px] ">
+          <tr>
+            <td className="p-2">
+              <div className="flex items-center justify-center gap-2">
+                <div className="w-8 h-8 bg-gray-200"></div>
+                <div>Product Name</div>
+              </div>
+            </td>
+            <td className="p-2">05.11.21</td>
+            <td className="p-2">008100471641</td>
+            <td className="p-2">Mini Object</td>
+            <td className="p-2">4</td>
+            <td className="p-2">John Mayer</td>
+            <td className="p-2">USA, New York</td>
+            <td className="p-2">35 USD</td>
+            <td className="p-2">Processing</td>
+            <td className="p-2">...</td>
+          </tr>
+          <tr>
+            <td className="p-2">
+              <div className="flex items-center justify-center gap-2">
+                <div className="w-8 h-8 bg-gray-200"></div>
+                <div>Product Name</div>
+              </div>
+            </td>
+            <td className="p-2">05.11.21</td>
+            <td className="p-2">008100471641</td>
+            <td className="p-2">Mini Object</td>
+            <td className="p-2">4</td>
+            <td className="p-2">John Mayer</td>
+            <td className="p-2">USA, New York</td>
+            <td className="p-2">35 USD</td>
+            <td className="p-2">Processing</td>
+            <td className="p-2">...</td>
+          </tr>
+          <tr>
+            <td className="p-2">
+              <div className="flex items-center justify-center gap-2">
+                <div className="w-8 h-8 bg-gray-200"></div>
+                <div>Product Name</div>
+              </div>
+            </td>
+            <td className="p-2">05.11.21</td>
+            <td className="p-2">008100471641</td>
+            <td className="p-2">Mini Object</td>
+            <td className="p-2">4</td>
+            <td className="p-2">John Mayer</td>
+            <td className="p-2">USA, New York</td>
+            <td className="p-2">35 USD</td>
+            <td className="p-2">Processing</td>
+            <td className="p-2">...</td>
+          </tr>
+          <tr>
+            <td className="p-2">
+              <div className="flex items-center justify-center gap-2">
+                <div className="w-8 h-8 bg-gray-200"></div>
+                <div>Product Name</div>
+              </div>
+            </td>
+            <td className="p-2">05.11.21</td>
+            <td className="p-2">008100471641</td>
+            <td className="p-2">Mini Object</td>
+            <td className="p-2">4</td>
+            <td className="p-2">John Mayer</td>
+            <td className="p-2">USA, New York</td>
+            <td className="p-2">35 USD</td>
+            <td className="p-2">Processing</td>
+            <td className="p-2">...</td>
+          </tr>
+          <tr>
+            <td className="p-2">
+              <div className="flex items-center justify-center gap-2">
+                <div className="w-8 h-8 bg-gray-200"></div>
+                <div>Product Name</div>
+              </div>
+            </td>
+            <td className="p-2">05.11.21</td>
+            <td className="p-2">008100471641</td>
+            <td className="p-2">Mini Object</td>
+            <td className="p-2">4</td>
+            <td className="p-2">John Mayer</td>
+            <td className="p-2">USA, New York</td>
+            <td className="p-2">35 USD</td>
+            <td className="p-2">Processing</td>
+            <td className="p-2">...</td>
+          </tr>
+          <tr>
+            <td className="p-2">
+              <div className="flex items-center justify-center gap-2">
+                <div className="w-8 h-8 bg-gray-200"></div>
+                <div>Product Name</div>
+              </div>
+            </td>
+            <td className="p-2">05.11.21</td>
+            <td className="p-2">008100471641</td>
+            <td className="p-2">Mini Object</td>
+            <td className="p-2">4</td>
+            <td className="p-2">John Mayer</td>
+            <td className="p-2">USA, New York</td>
+            <td className="p-2">35 USD</td>
+            <td className="p-2">Processing</td>
+            <td className="p-2">...</td>
+          </tr>
+          <tr>
+            <td className="p-2">
+              <div className="flex items-center justify-center gap-2">
+                <div className="w-8 h-8 bg-gray-200"></div>
+                <div>Product Name</div>
+              </div>
+            </td>
+            <td className="p-2">05.11.21</td>
+            <td className="p-2">008100471641</td>
+            <td className="p-2">Mini Object</td>
+            <td className="p-2">4</td>
+            <td className="p-2">John Mayer</td>
+            <td className="p-2">USA, New York</td>
+            <td className="p-2">35 USD</td>
+            <td className="p-2">Processing</td>
+            <td className="p-2">...</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Orders;
--- app/pages/products/page.jsx
+++ app/pages/products/page.jsx
@@ -1,5 +1,20 @@
+import SearchBar from "@/app/components/SearchBar";
+
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div>
+      <SearchBar />
+
+      <div className="flex justify-end mt-10 gap-5">
+        <button className="bg-accent text-white p-2 rounded-xl shadow-lg">
+          Add Order+
+        </button>
+        <button className="bg-accent text-white p-2 rounded-xl shadow-lg">
+          Export CVS
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
